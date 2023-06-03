aa990cdd4952c0f0519e8f49b98fa42b6411f66c
MatheusPeretta <matheusperetta.saopaulo@gmail.com>
2023-06-03 17:21:47 -0300

    versão 001.2

--- js/disponiveis.js
+++ js/disponiveis.js
@@ -13,7 +13,40 @@ function fecharInbox(){
 }
 
 function disponibilizar(){
-     window.alert('Parabéns!!! Você se disponibilizou para uma carona.')
+    let partida = document.getElementById('partida').value
+    let destino = document.getElementById('destino').value
+    let dataCarona = document.getElementById('dtCarona').value
+    let horario = document.getElementById('hrCarona').value
+
+    if(partida.length === 0){
+        window.alert('preencha todos os campos')
+    } else if(destino.length === 0){
+        window.alert('preencha todos os campos')
+    } else if(dataCarona.length === 0){
+        window.alert('preencha todos os campos')
+    } else if(horario.length == 0){
+        window.alert('preencha todos os campos')
+    } else{
+        window.alert('Parabéns!!! Você se disponibilizou para uma carona.')
+
+        document.getElementById('ijadisponiveis').innerHTML += `<table class="table table-success table-striped" style="margin-top: 10px;">
+        <tbody>
+            <tr>
+            <th scope="row" style="padding: 0.5rem 1.5rem;">Partida de:</th>
+            <th scope="row" style="padding: 0.5rem 1.5rem;">Destino:</th>
+            <th scope="row" style="padding: 0.5rem 1.5rem;">Saída:</th>
+            <th scope="row" style="padding: 0.5rem 1.5rem;">Data:</th>
+            <th scope="row" style="padding: 0.5rem 1.5rem;">Combinar:</th>
+            </tr>
+            <tr>
+                <td style="padding: 0.5rem 1.5rem;">${partida}</td>
+                <td style="padding: 0.5rem 1.5rem;">${destino}</td>
+                <td style="padding: 0.5rem 1.5rem;">${horario}</td>
+                <td style="padding: 0.5rem 1.5rem;">${dataCarona}</td>
+                <td style="padding: 0.5rem 1.5rem;"><button id="agendar4" onclick="agendar()">Agendar</button> <button id="abrirmensagem" onclick="aparecerInbox()">Mensagem</button></td>
+            </tr>
+        </table> `
+    }
 }
 
 function agendar(){
